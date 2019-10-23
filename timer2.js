a1c5a583621dac850e6bf20ea3731ac8ba388fc6
ZSully09 <zsullivan93@gmail.com>
2019-10-23 01:59:32 +0000

    timer2 functionality commit

--- timer2.js
+++ timer2.js
@@ -1,3 +1,4 @@
+const stdOut = process.stdout;
 const stdin = process.stdin;
 // don't worry about these next two lines of setup work.
 stdin.setRawMode(true);
@@ -7,12 +8,28 @@ stdin.setEncoding("utf8");
 // Event Handling for User Input
 ////////////
 
-// on any input from stdin (standard input), output a "." to stdout
 stdin.on("data", key => {
   if (key === "\u0003") {
+    console.log("Thanks for using me, ciao!");
     process.exit();
   }
-  process.stdout.write(".");
+  if (key === "b") {
+    stdOut.write("\x07");
+  }
+  if (
+    key === "1" ||
+    key === "2" ||
+    key === "3" ||
+    key === "4" ||
+    key === "5" ||
+    key === "6" ||
+    key === "7" ||
+    key === "8" ||
+    key === "9"
+  ) {
+    stdOut.write(`setting timer for ${key} seconds...\n`);
+    setTimeout(() => {
+      stdOut.write("\x07");
+    }, key * 1000);
+  }
 });
-
-console.log("after callback");
